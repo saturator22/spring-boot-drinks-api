145c57b594bfa347b1710d61ba37743c39413b06
saturator22 <f.szczeski@gmail.com>
2018-10-01 18:41:18 +0200

    Implement Liquor repository with findall() method

--- src/main/java/com/codecool/springbootdrinks/Repository/LiquorRepository.java
+++ src/main/java/com/codecool/springbootdrinks/Repository/LiquorRepository.java
@@ -1,4 +1,15 @@
 package com.codecool.springbootdrinks.Repository;
 
-public class LiquorRepository {
+import com.codecool.springbootdrinks.Model.Liquor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LiquorRepository extends JpaRepository<Liquor, Long> {
+
+    List<Liquor> findAll();
 }
