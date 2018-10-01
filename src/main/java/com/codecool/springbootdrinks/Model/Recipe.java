cdca8c1ed4e75b7941afd1a34af916e3d703e3cb
saturator22 <f.szczeski@gmail.com>
2018-10-01 15:32:54 +0200

    Annotate Recipe class

--- src/main/java/com/codecool/springbootdrinks/Model/Recipe.java
+++ src/main/java/com/codecool/springbootdrinks/Model/Recipe.java
@@ -1,6 +1,10 @@
 package com.codecool.springbootdrinks.Model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "recipes")
@@ -8,9 +12,23 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
-    int id;
+    long id;
 
     @Column
-    @ManyToMany
-    int[] liquor_ids;
+    String name;
+    @Column
+    String description;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            mappedBy = "recipeList")
+    List<Liquor> liquorList;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "type_id", referencedColumnName = "type_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Type type;
 }
