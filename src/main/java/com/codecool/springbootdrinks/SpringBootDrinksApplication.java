79c91e63718cc062abb4678961e20bb0b0468990
saturator22 <f.szczeski@gmail.com>
2018-10-01 12:56:29 +0200

    Init commit

---
+++ src/main/java/com/codecool/springbootdrinks/SpringBootDrinksApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.springbootdrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDrinksApplication.class, args);
+	}
+}
