760a7773af56e8cfdfa6c4bc74abf62d1a9bd378
saturator22 <f.szczeski@gmail.com>
2018-10-01 18:43:31 +0200

    Add CommandLineRunner to create test entries in database

--- src/main/java/com/codecool/springbootdrinks/SpringBootDrinksApplication.java
+++ src/main/java/com/codecool/springbootdrinks/SpringBootDrinksApplication.java
@@ -1,7 +1,10 @@
 package com.codecool.springbootdrinks;
 
+import com.codecool.springbootdrinks.Service.LiquorService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootDrinksApplication {
@@ -9,4 +12,12 @@ public class SpringBootDrinksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDrinksApplication.class, args);
 	}
+
+//	@Bean
+//	public CommandLineRunner demo(LiquorService liquorService) {
+//		return (args) -> {
+//			liquorService.addData();
+//		};
+//	}
 }
+
