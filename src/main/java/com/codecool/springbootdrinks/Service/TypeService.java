0616c9fe0d8b8ea8d3c8616664022b4113f47032
Bart <bartpetka@gmail.com>
2018-10-02 14:56:12 +0200

    Implement TypeService (get methods)

--- src/main/java/com/codecool/springbootdrinks/Service/TypeService.java
+++ src/main/java/com/codecool/springbootdrinks/Service/TypeService.java
@@ -1,8 +1,11 @@
 package com.codecool.springbootdrinks.Service;
 
+import com.codecool.springbootdrinks.Model.Type;
 import com.codecool.springbootdrinks.Repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TypeService {
@@ -13,4 +16,22 @@ public class TypeService {
         this.typeRepository = typeRepository;
     }
 
+    public List<Type> findAll() {
+        return typeRepository.findAll();
+    }
+
+    public Type findTypeByTypeId(Long id) {
+        return typeRepository.findTypeByTypeId(id);
+    }
+
+    public Type addType(Type type) {
+        return typeRepository.save(type);
+    }
+
+    public Type editType(Long typeId, Type requestedType) {
+        Type loadedType = typeRepository.findTypeByTypeId(typeId);
+        loadedType.setTypeName(requestedType.getTypeName());
+        loadedType.setRecipes(requestedType.getRecipes());
+        return typeRepository.save(loadedType);
+    }
 }
