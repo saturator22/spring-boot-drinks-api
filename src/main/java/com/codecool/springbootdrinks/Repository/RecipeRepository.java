98cd7b4b9252482188970a8a02492cf5fcff7da9
Bart <bartpetka@gmail.com>
2018-10-02 11:18:57 +0200

    Implement findById method

--- src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
+++ src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
@@ -1,10 +1,13 @@
 package com.codecool.springbootdrinks.Repository;
 
 import com.codecool.springbootdrinks.Model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findAll();
+
+    Recipe findRecipeById(Long id);
 }
