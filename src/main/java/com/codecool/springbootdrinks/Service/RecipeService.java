29eccc7074f7cc0c6f87f220328fdb57e14fea53
Bart <bartpetka@gmail.com>
2018-10-02 11:16:23 +0200

    Implement recipeService with getMethods

---
+++ src/main/java/com/codecool/springbootdrinks/Service/RecipeService.java
@@ -0,0 +1,26 @@
+package com.codecool.springbootdrinks.Service;
+
+import com.codecool.springbootdrinks.Model.Recipe;
+import com.codecool.springbootdrinks.Repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecipeService {
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<Recipe> findAll() {
+        return recipeRepository.findAll();
+    }
+
+    public Recipe findByRecipeId(Long recipeId) {
+        return recipeRepository.findRecipeById(recipeId);
+    }
+}
