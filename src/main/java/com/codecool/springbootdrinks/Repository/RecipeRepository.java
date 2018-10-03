1def1c69df3ecddacb8d130458a52c79eeb6c3dc
Bart <bartpetka@gmail.com>
2018-10-03 12:17:56 +0200

    Implement named querry for deleting booze from recipe

--- src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
+++ src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
@@ -2,11 +2,14 @@ package com.codecool.springbootdrinks.Repository;
 
 import com.codecool.springbootdrinks.Model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findAll();
     Recipe findRecipeById(Long id);
+    @Query(value = "DELETE FROM liquors_receipes WHERE liquor_id = :liq_id AND recipe_id = :rec_id", nativeQuery = true)
+    void deleteLiquorFromRecipe(@Param("liq_id")Long liq_id, @Param("rec_id")Long rec_id);
 }
