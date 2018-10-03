d2ea548889feaf004a62e77a480c32e3d50ad8ad
Bart <bartpetka@gmail.com>
2018-10-03 14:05:59 +0200

    implement named Querry for updating liquors lists

--- src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
+++ src/main/java/com/codecool/springbootdrinks/Repository/RecipeRepository.java
@@ -2,6 +2,7 @@ package com.codecool.springbootdrinks.Repository;
 
 import com.codecool.springbootdrinks.Model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,6 +11,12 @@ import java.util.List;
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findAll();
     Recipe findRecipeById(Long id);
+
+    @Modifying
     @Query(value = "DELETE FROM liquors_receipes WHERE liquor_id = :liq_id AND recipe_id = :rec_id", nativeQuery = true)
     void deleteLiquorFromRecipe(@Param("liq_id")Long liq_id, @Param("rec_id")Long rec_id);
+
+    @Modifying
+    @Query(value = "INSERT INTO liquors_receipes (liquor_id, recipe_id) VALUES (:liq_id, :rec_id);", nativeQuery = true)
+    void addLiquorToRecipe(@Param("liq_id")Long liq_id, @Param("rec_id")Long rec_id);
 }
