47a8a43661a48efb36b203b54693c66e120d2911
saturator22 <f.szczeski@gmail.com>
2018-10-04 13:33:03 +0200

    Adjust method names switched in Repository

--- src/main/java/com/codecool/springbootdrinks/Service/LiquorService.java
+++ src/main/java/com/codecool/springbootdrinks/Service/LiquorService.java
@@ -9,7 +9,6 @@ import com.codecool.springbootdrinks.Repository.TypeRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,23 +33,26 @@ public class LiquorService {
 
     public List<Liquor> getAllLiquors() {
         log4j.info("Get ALL Liquors");
-        return liquorRepository.findAll();
+        return liquorRepository.findAllByArchivedFalse();
     }
 
     public Liquor getLiquorById(Long id) {
-        return liquorRepository.findLiquorByLiquorId(id);
+        log4j.info("Get liquor with id " + id);
+        return liquorRepository.findLiquorByLiquorIdAndArchivedFalse(id);
     }
 
     public Liquor createLiquor(Liquor liquor) {
+        log4j.info("Created object: " + liquor);
         return liquorRepository.save(liquor);
     }
 
-    public ResponseEntity<?> deleteLiquor(Long liquorId) {
+    public Liquor deleteLiquor(Long liquorId) {
         return liquorRepository.findById(liquorId)
                 .map(liquor -> {
-                    liquorRepository.delete(liquor);
-                return ResponseEntity.ok().build();})
-                .orElseThrow(() -> new IllegalArgumentException("There is no resource like that"));
+                    liquor.setIsArchived(true);
+                    return liquorRepository.save(liquor);
+                }).orElseThrow(() -> {log4j.error("Failed to delete object with id: " + liquorId);
+                return new IllegalArgumentException("There is no object with id: " + liquorId);});
     }
 
     public Liquor updateLiquor(Long liquorId, Liquor liquorRequest) {
@@ -59,33 +61,35 @@ public class LiquorService {
                     liquor.setCategory(liquorRequest.getCategory());
                     liquor.setName(liquorRequest.getName());
                     liquor.setRecipeList(liquorRequest.getRecipeList());
+                    liquor.setIsArchived(liquorRequest.getIsArchived());
                     return liquorRepository.save(liquor);
-                }).orElseThrow(() -> new IllegalArgumentException("There is no resource like that"));
+                }).orElseThrow(() -> {log4j.error("Failed to update on resource with id: " + liquorId);
+                    return new IllegalArgumentException("There is no resource like that");});
     }
 
-//    public void addData() {
-//        Liquor liquor = new Liquor("Whisky", "Johnny Walker");
-//        Liquor liquor2 = new Liquor("Whisky", "Ballantines");
-//        Liquor liquor1 = new Liquor("Martini", "Bianco");
-//        Recipe recipe = new Recipe("Whisky Sour", "Whisky 50 ml,\nEggWhite,\nLemon Juice");
-//        Recipe recipe1 = new Recipe("Vodka Martini", "Martini Bianco 80 ml\nVodka 50 ml\nLemon or Olive");
-//        Type type = new Type("Short");
-//        Type type1 = new Type("Tini");
-//
-//        typeRepository.save(type);
-//        typeRepository.save(type1);
-//
-//        liquor.getRecipeList().add(recipe);
-//        liquor2.getRecipeList().add(recipe);
-//        liquor1.getRecipeList().add(recipe1);
-//        recipe.getLiquorList().add(liquor);
-//        recipe.getLiquorList().add(liquor2);
-//        recipe1.getLiquorList().add(liquor1);
-//        recipe.setType(type);
-//        recipe1.setType(type1);
-//
-//        liquorRepository.save(liquor);
-//        liquorRepository.save(liquor1);
-//        liquorRepository.save(liquor2);
-//    }
+    public void addData() {
+        Liquor liquor = new Liquor("Whisky", "Johnny Walker");
+        Liquor liquor2 = new Liquor("Whisky", "Ballantines");
+        Liquor liquor1 = new Liquor("Martini", "Bianco");
+        Recipe recipe = new Recipe("Whisky Sour", "Whisky 50 ml,\nEggWhite,\nLemon Juice");
+        Recipe recipe1 = new Recipe("Vodka Martini", "Martini Bianco 80 ml\nVodka 50 ml\nLemon or Olive");
+        Type type = new Type("Short");
+        Type type1 = new Type("Tini");
+
+        typeRepository.save(type);
+        typeRepository.save(type1);
+
+        liquor.getRecipeList().add(recipe);
+        liquor2.getRecipeList().add(recipe);
+        liquor1.getRecipeList().add(recipe1);
+        recipe.getLiquorList().add(liquor);
+        recipe.getLiquorList().add(liquor2);
+        recipe1.getLiquorList().add(liquor1);
+        recipe.setType(type);
+        recipe1.setType(type1);
+
+        liquorRepository.save(liquor);
+        liquorRepository.save(liquor1);
+        liquorRepository.save(liquor2);
+    }
 }
